562ba97dc3538417c6d811570908aec5043f89e9
KhatriSir <88650182+KhatriSir@users.noreply.github.com>
2021-08-19 14:43:17 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+const url = require('url')
+const shell = require('electron').shell
+const globalShortcut = require('electron').globalShortcut
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+ createWindow = () => {
+    // Create the browser window.
+    win = new BrowserWindow({ resizable: false, width: 800, height: 660 })
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'src/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+    //***//
+        //***//
+        globalShortcut.register('f5', () => {
+            console.log('f5 is pressed')
+            win.reload()
+        })
+        globalShortcut.register('CommandOrControl+R', () => {
+            console.log('CommandOrControl+R is pressed')
+            win.reload()
+        })
+
+    // Open the DevTools.
+    // win.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    })
+
+    var menu = Menu.buildFromTemplate([{
+        label: 'Menu',
+        submenu: [
+            { label: 'Developer Profile',
+              click(){
+                shell.openExternal('https://github.com/jamesgeorge007')
+              }
+              },
+            {
+                label: 'Youtube',
+                click(){
+                    shell.openExternal('https://youtube.com')
+                }
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Exit',
+                click(){
+                    app.quit()
+                }
+            }
+        ]
+
+
+    }])
+
+    Menu.setApplicationMenu(menu);
+}
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
+})
